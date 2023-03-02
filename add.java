b0637d8a3e48395ef1655bcd486ce7415ce33a0b
Pavithra200595 <126651155+Pavithra200595@users.noreply.github.com>
2023-03-02 10:58:59 +0530

    changed b value to 2

--- add.java
+++ add.java
@@ -3,7 +3,7 @@ class arithmetic
     public static void main(String args[])
     {
         int a=20;
-        int b=5;
+        int b=2;
         System.out.println("addition of "+a+" and "+b+" is: " +(a+b));
     }
 }
