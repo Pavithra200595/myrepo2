46c7173a3bd5553707666825ca0f76b0d0bc26e7
pratheebapc <pratheebaravindran@gmail.com>
2023-03-02 12:10:11 +0530

    line 8 added

--- add.java
+++ add.java
@@ -2,8 +2,9 @@ class arithmetic
 {
     public static void main(String args[])
     {
-        int a=20;
-        int b=5;
+        int a=10;
+        int b=30;
         System.out.println("addition of "+a+" and "+b+" is: " +(a+b));
+        System.out.println("new line added");
     }
 }
