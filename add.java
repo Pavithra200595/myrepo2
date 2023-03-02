d77b848e9b9bd72e3cc28123999fe0a5981b889e
pavithra200595 <pavipavi491@gmail.com>
2023-03-02 10:40:12 +0530

    first commit

---
+++ add.java
@@ -0,0 +1,9 @@
+class arithmetic
+{
+    public static void main(String args[])
+    {
+        int a=20;
+        int b=5;
+        System.out.println("addition of "+a+" and "+b+" is: " +(a+b));
+    }
+}
